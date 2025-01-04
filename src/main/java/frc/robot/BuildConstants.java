72b286de9a733d8a238a605bb65f519e5236d718
PatribotsProgramming <patribotsprogramming4738@gmail.com>
2025-01-04 15:12:29 -0800

    SUPER MIGRATE!

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Swerve-Command-Based";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 61;
+  public static final String GIT_SHA = "81586bba18274c90e33e0b472eada57241793117";
+  public static final String GIT_DATE = "2024-12-05 00:07:07 PST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-12-10 17:05:49 PST";
+  public static final long BUILD_UNIX_TIME = 1733879149689L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
